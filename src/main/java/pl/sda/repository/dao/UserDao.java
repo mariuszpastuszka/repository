c3c19518b75d5456c29eabc7f79ac81535968c35
maniek <maniek@dev.pl>
2020-07-19 14:14:58 +0200

    [add] - SdaUser dao in progress...

---
+++ src/main/java/pl/sda/repository/dao/UserDao.java
@@ -0,0 +1,38 @@
+package pl.sda.repository.dao;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+import pl.sda.repository.domain.SdaUser;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * using jdbc template
+ */
+@Repository
+@Slf4j
+public class UserDao {
+
+    // CREATE/UPDATE
+    public SdaUser saveUser(SdaUser sdaUser) {
+        log.info("saveUser() - argument: [{}]", sdaUser);
+        return sdaUser;
+    }
+
+    public List<SdaUser> readAllUsers() {
+        log.info("readAllUsers()");
+        return Collections.emptyList();
+    }
+
+    public Optional<SdaUser> findUserByPesel(String pesel) {
+        log.info("findUserByPesel() - pesel: [{}]", pesel);
+        return Optional.empty();
+    }
+
+    public boolean deleteUserByPesel(String pesel) {
+        log.info("deleteUserByPesel() - pesel: [{}]", pesel);
+        return false;
+    }
+}
